5ca90a3ab7bf1478e9fa5468acdf11203e08a563
Jeremy Clark <clark.jm.72@gmail.com>
2020-06-25 15:08:12 -0400

    Formatting with different sizes

--- src/components/RequestForm/RequestForm.tsx
+++ src/components/RequestForm/RequestForm.tsx
@@ -22,8 +22,8 @@ export const RequestForm: React.FunctionComponent<any> = (props) => {
             <h1>New Request</h1>
             <Form className="request-form m-3">
                 <Form.Row className="mb-3">
-                    <Col>
-                        <Form.Label>Requester Org Symbol</Form.Label>
+                    <Col xl="4" lg="4" md="6" sm="6" xs="12">
+                        <Form.Label lg="4" sm="6">Requester Org Symbol</Form.Label>
                         <Form.Control
                             type="text"
                             placeholder="Your Org Symbol"
@@ -31,8 +31,8 @@ export const RequestForm: React.FunctionComponent<any> = (props) => {
                             onChange={e => updateRequest('RequesterOrgSymbol', e.target.value)}
                         />
                     </Col>
-                    <Col>
-                        <Form.Label>Requester DSN #</Form.Label>
+                    <Col xl="4" lg="4" md="6" sm="6" xs="12">
+                        <Form.Label lg="4" sm="6">Requester DSN #</Form.Label>
                         <Form.Control
                             type="text"
                             placeholder="Your DSN Phone Number"
@@ -40,8 +40,8 @@ export const RequestForm: React.FunctionComponent<any> = (props) => {
                             onChange={e => updateRequest('RequesterDSNPhone', e.target.value)}
                         />
                     </Col>
-                    <Col>
-                        <Form.Label>Requester Comm #</Form.Label>
+                    <Col xl="4" lg="4" md="6" sm="6" xs="12">
+                        <Form.Label lg="4" sm="6">Requester Comm #</Form.Label>
                         <Form.Control
                             type="text"
                             placeholder="Your Commercial Phone Number"
@@ -51,7 +51,7 @@ export const RequestForm: React.FunctionComponent<any> = (props) => {
                     </Col>
                 </Form.Row>
                 <Form.Row className="mb-3">
-                    <Col>
+                    <Col xl="6" lg="6" md="8" sm="8" xs="12">
                         <Form.Label>2 Ltr/PEO to Approve:</Form.Label>
                         <Form.Control
                             as={PeoplePicker}
@@ -63,7 +63,7 @@ export const RequestForm: React.FunctionComponent<any> = (props) => {
                             required={true}
                         />
                     </Col>
-                    <Col>
+                    <Col xl="4" lg="4" md="4" sm="6" xs="12">
                         <Form.Label>PEO Org Symbol</Form.Label>
                         <Form.Control
                             type="text"
@@ -72,9 +72,7 @@ export const RequestForm: React.FunctionComponent<any> = (props) => {
                             onChange={e => updateRequest('PEOOrgSymbol', e.target.value)}
                         />
                     </Col>
-                </Form.Row>
-                <Form.Row className="mb-3">
-                    <Col>
+                    <Col xl="4" lg="4" md="6" sm="6" xs="12">
                         <Form.Label>PEO DSN #</Form.Label>
                         <Form.Control
                             type="text"
@@ -83,7 +81,7 @@ export const RequestForm: React.FunctionComponent<any> = (props) => {
                             onChange={e => updateRequest('PEO_DSNPhone', e.target.value)}
                         />
                     </Col>
-                    <Col>
+                    <Col xl={{ span: 4, offset: 2 }} lg={{ span: 4, offset: 2 }} md="6" sm="6" xs="12">
                         <Form.Label>PEO Comm #</Form.Label>
                         <Form.Control
                             type="text"
@@ -94,54 +92,56 @@ export const RequestForm: React.FunctionComponent<any> = (props) => {
                     </Col>
                 </Form.Row>
                 <Form.Row className="mb-3">
-                    <Form.Label>Requirement Title</Form.Label>
-                    <Form.Control
-                        type="text"
-                        placeholder="Title of the Requirement being requested"
-                        value={request.Title}
-                        onChange={e => updateRequest('Title', e.target.value)}
-                    />
-                </Form.Row>
-                <Form.Row className="mb-3">
-                    <Form.Label className="mr-3 mb-0">Requirement Type:</Form.Label>
-                    <Form.Check inline label={RequirementTypes.NEW_CAP} type="radio" id="new-capability-radio"
-                        checked={request.RequirementType === RequirementTypes.NEW_CAP}
-                        onClick={() => updateRequest("RequirementType", RequirementTypes.NEW_CAP)}
-                    />
-                    <Form.Check inline label={RequirementTypes.MOD_EXISTING_CAP} type="radio" id="modification-radio"
-                        checked={request.RequirementType === RequirementTypes.MOD_EXISTING_CAP}
-                        onClick={() => updateRequest("RequirementType", RequirementTypes.MOD_EXISTING_CAP)}
-                    />
-                    <Form.Check inline label={RequirementTypes.FUNCTIONAL} type="radio" id="functional-radio"
-                        checked={request.RequirementType === RequirementTypes.FUNCTIONAL}
-                        onClick={() => updateRequest("RequirementType", RequirementTypes.FUNCTIONAL)}
-                    />
-                    <Form.Check inline label={RequirementTypes.NON_FUNCTIONAL} type="radio" id="non-functional-radio"
-                        checked={request.RequirementType === RequirementTypes.NON_FUNCTIONAL}
-                        onClick={() => updateRequest("RequirementType", RequirementTypes.NON_FUNCTIONAL)}
-                    />
-                </Form.Row>
-                <Form.Row className="mb-3">
-                    <Col>
+                    <Col xl="8" lg="8" md="10" sm="12" xs="12">
+                        <Form.Label>Requirement Title</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder="Title of the Requirement being requested"
+                            value={request.Title}
+                            onChange={e => updateRequest('Title', e.target.value)}
+                        />
+                    </Col>
+                    <Col xl="12" lg="12" md="12" sm="12" xs="12">
+                        <Form.Label className="mr-3 mb-0">Requirement Type:</Form.Label>
+                        <Form.Check inline label={RequirementTypes.NEW_CAP} type="radio" id="new-capability-radio"
+                            checked={request.RequirementType === RequirementTypes.NEW_CAP}
+                            onClick={() => updateRequest("RequirementType", RequirementTypes.NEW_CAP)}
+                        />
+                        <Form.Check inline label={RequirementTypes.MOD_EXISTING_CAP} type="radio" id="modification-radio"
+                            checked={request.RequirementType === RequirementTypes.MOD_EXISTING_CAP}
+                            onClick={() => updateRequest("RequirementType", RequirementTypes.MOD_EXISTING_CAP)}
+                        />
+                        <Form.Check inline label={RequirementTypes.FUNCTIONAL} type="radio" id="functional-radio"
+                            checked={request.RequirementType === RequirementTypes.FUNCTIONAL}
+                            onClick={() => updateRequest("RequirementType", RequirementTypes.FUNCTIONAL)}
+                        />
+                        <Form.Check inline label={RequirementTypes.NON_FUNCTIONAL} type="radio" id="non-functional-radio"
+                            checked={request.RequirementType === RequirementTypes.NON_FUNCTIONAL}
+                            onClick={() => updateRequest("RequirementType", RequirementTypes.NON_FUNCTIONAL)}
+                        />
+                    </Col>
+                    <Col className="mt-4 mb-4" xl="4" lg="4" md="6" sm="6" xs="12">
                         <Form.Check inline label="Is Requirement Funded?" type="checkbox" id="funded-checkbox"
                             checked={showFundingField}
                             onClick={flipShowFundingField}
                         />
                     </Col>
-                    {showFundingField &&
-                        <Col>
-                            <Form.Label>If Yes, Org/PEO funding it:</Form.Label>
-                            <Form.Control
-                                type="text"
-                                placeholder="Org/PEO Funding the Requirement"
-                                value={request.FundingOrgOrPEO}
-                                onChange={e => updateRequest('FundingOrgOrPEO', e.target.value)}
-                            />
-                        </Col>
-                    }
+                    <Col xl="4" lg="4" md="6" sm="6" xs="12">
+                        {showFundingField &&
+                            <>
+                                <Form.Label>If Yes, Org/PEO funding it:</Form.Label>
+                                <Form.Control
+                                    type="text"
+                                    placeholder="Org/PEO Funding the Requirement"
+                                    value={request.FundingOrgOrPEO}
+                                    onChange={e => updateRequest('FundingOrgOrPEO', e.target.value)}
+                                />
+                            </>
+                        }
+                    </Col>
                 </Form.Row>
                 <Form.Row className="mb-3">
-                    <Col>
+                    <Col xl="4" lg="4" md="6" sm="6" xs="12">
                         <Form.Label>Application Needed:</Form.Label>
                         <Form.Control
                             as="select"
@@ -151,28 +151,30 @@ export const RequestForm: React.FunctionComponent<any> = (props) => {
                             {Object.values(ApplicationTypes).map(type => <option>{type}</option>)}
                         </Form.Control>
                     </Col>
-                    {request.ApplicationNeeded === ApplicationTypes.OTHER &&
-                        <Col>
-                            <Form.Label>If Other, please name:</Form.Label>
-                            <Form.Control
-                                type="text"
-                                placeholder="Other Application Needed"
-                                value={request.OtherApplicationNeeded}
-                                onChange={e => updateRequest('OtherApplicationNeeded', e.target.value)}
-                            />
-                        </Col>
-                    }
+                    <Col xl="4" lg="4" md="6" sm="6" xs="12">
+                        {request.ApplicationNeeded === ApplicationTypes.OTHER &&
+                            <>
+                                <Form.Label>If Other, please name:</Form.Label>
+                                <Form.Control
+                                    type="text"
+                                    placeholder="Other Application Needed"
+                                    value={request.OtherApplicationNeeded}
+                                    onChange={e => updateRequest('OtherApplicationNeeded', e.target.value)}
+                                />
+                            </>
+                        }
+                    </Col>
                 </Form.Row>
                 <Form.Row className="mb-3">
-                    <Col><Form.Label>Projected Organizations Impacted:</Form.Label></Col>
-                    <Col>
-                        <Form.Check inline label="Enterprise" type="checkbox" id="enterprise-checkbox"
+                    <Col xl="3" lg="3" md="6" sm="6" xs="12"><Form.Label>Projected Organizations Impacted:</Form.Label></Col>
+                    <Col className="request-vertical-center" xl="2" lg="3" md="6" sm="6" xs="12">
+                        <Form.Check inline label="Is Enterprise?" type="checkbox" id="enterprise-checkbox"
                             checked={request.IsProjectedOrgsEnterprise}
                             onClick={() => updateRequest('IsProjectedOrgsEnterprise', !request.IsProjectedOrgsEnterprise)}
                         />
                     </Col>
-                    <Col>
-                        <Form.Label>Center:</Form.Label>
+                    <Col xl="3" lg="3" md="6" sm="6" xs="12">
+                        <Form.Label>Impacted Center:</Form.Label>
                         <Form.Control
                             as="select"
                             value={request.ProjectedOrgsImpactedCenter}
@@ -181,8 +183,8 @@ export const RequestForm: React.FunctionComponent<any> = (props) => {
                             {Object.values(Centers).map(center => <option>{center}</option>)}
                         </Form.Control>
                     </Col>
-                    <Col>
-                        <Form.Label>Org:</Form.Label>
+                    <Col xl="3" lg="3" md="6" sm="6" xs="12">
+                        <Form.Label>Impacted Org:</Form.Label>
                         <Form.Control
                             type="text"
                             placeholder="Project Org Impacted"
