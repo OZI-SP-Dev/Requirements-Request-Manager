fcf253c1ce55dc6b4d5b734fda71f23d562bcb2d
Jeremy Clark <clark.jm.72@gmail.com>
2020-07-07 15:08:22 -0400

    accordion on the table, but it doesn't really work

--- src/components/Requests/Requests.tsx
+++ src/components/Requests/Requests.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Container, Table } from "react-bootstrap";
+import { Button, Container, Table, Accordion } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { IRequirementsRequestCRUD } from "../../api/DomainObjects";
 
@@ -10,14 +10,13 @@ export interface IRequestsProps {
 export const Requests: React.FunctionComponent<IRequestsProps> = (props) => {
 
     return (
-        <Container className="pb-5 pt-3">
+        <Container fluid="md" className="pb-5 pt-3">
             <h1>Requests</h1>
-            <Table striped bordered hover responsive>
+            <Table bordered hover responsive>
                 <thead>
                     <tr>
-                        <th>ID</th>
-                        <th>Requester</th>
                         <th>Title</th>
+                        <th>Requester</th>
                         <th>Request Date</th>
                         <th>Requirement Type</th>
                         <th>Application Needed</th>
@@ -25,20 +24,22 @@ export const Requests: React.FunctionComponent<IRequestsProps> = (props) => {
                         <th>Operational Need Date</th>
                     </tr>
                 </thead>
-                <tbody>
-                    {props.requests.map(request =>
-                        <tr key={request.Id}>
-                            <td>{request.Id}</td>
-                            <td>{request.Requester.Title}</td>
+                {props.requests.map(request =>
+                    <Accordion as='tbody' key={request.Id}>
+                        <Accordion.Toggle eventKey="0" as='tr'>
                             <td>{request.Title}</td>
+                            <td>{request.Requester.Title}</td>
                             <td>{request.RequestDate.format("DD MMM YYYY")}</td>
                             <td>{request.RequirementType}</td>
                             <td>{request.ApplicationNeeded}</td>
                             <td>{request.OrgPriority}</td>
                             <td>{request.OperationalNeedDate.format("DD MMM YYYY")}</td>
-                        </tr>
-                    )}
-                </tbody>
+                        </Accordion.Toggle>
+                        <Accordion.Collapse eventKey="0">
+                            <span>Inside the collapse asdff asdf asdfas fasdf asd fasd fasdf asd f</span>
+                        </Accordion.Collapse>
+                    </Accordion>
+                )}
             </Table>
             <Link to="/Requests/new">
                 <Button variant="primary" className="float-left">New Request</Button>
