2cff4349f00084194982457abd8b9a95b6c02dd8
Jeremy Clark <clark.jm.72@gmail.com>
2020-07-28 12:39:52 -0400

    Added colons on new/edit page

--- src/components/RequestForm/RequestForm.tsx
+++ src/components/RequestForm/RequestForm.tsx
@@ -166,7 +166,7 @@ export const RequestForm: React.FunctionComponent<IRequestFormProps> = (props) =
                         />
                     </Col>
                     <Col xl="4" lg="4" md="4" sm="6" xs="12">
-                        <Form.Label>2 Ltr/PEO Org Symbol</Form.Label>
+                        <Form.Label>2 Ltr/PEO Org Symbol:</Form.Label>
                         <Form.Control
                             type="text"
                             placeholder="Approving PEO's Org Symbol"
@@ -176,7 +176,7 @@ export const RequestForm: React.FunctionComponent<IRequestFormProps> = (props) =
                         />
                     </Col>
                     <Col xl="4" lg="4" md="6" sm="6" xs="12">
-                        <Form.Label>2 Ltr/PEO DSN #</Form.Label>
+                        <Form.Label>2 Ltr/PEO DSN #:</Form.Label>
                         <Form.Control
                             type="text"
                             placeholder="Approving PEO's DSN Phone Number"
@@ -186,7 +186,7 @@ export const RequestForm: React.FunctionComponent<IRequestFormProps> = (props) =
                         />
                     </Col>
                     <Col xl={{ span: 4, offset: 2 }} lg={{ span: 4, offset: 2 }} md="6" sm="6" xs="12">
-                        <Form.Label>2 Ltr/PEO Comm #</Form.Label>
+                        <Form.Label>2 Ltr/PEO Comm #:</Form.Label>
                         <Form.Control
                             type="text"
                             placeholder="Approving PEO's Commercial Phone Number"
@@ -198,7 +198,7 @@ export const RequestForm: React.FunctionComponent<IRequestFormProps> = (props) =
                 </Form.Row>}
                 <Form.Row>
                     <Col xl="6" lg="6" md="8" sm="12" xs="12">
-                        <Form.Label>Requirement Title</Form.Label>
+                        <Form.Label>Requirement Title:</Form.Label>
                         <Form.Control
                             type="text"
                             placeholder="Title of the Requirement being requested"
